c1c41ccc4895eda4b2564c1cb93f64ae0311898a
SillyCiely <sillyciely@gmail.com>
2024-05-25 16:07:30 +1000

    linked navbar buttons to pages

---
+++ Frontend/osrs-app/src/pages/HomePage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import PricingTableComponent from "../components/PricingTableComponent";
+
+const HomePage = () => {
+    return (
+        <div style={{ margin: '20px' }}>
+            <h1>Home Page Placeholder</h1>
+            <p>Click on the "Log in" button in the top right corner to log in.</p>
+
+            <PricingTableComponent />
+        </div>
+    );
+};
+
+export default HomePage;
